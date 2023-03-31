545e2a37a1c926f404b06e5bd389faebcc6a9d1e
Javi <javiermoyasimo@gmail.com>
2023-03-31 16:06:44 +0200

    first project create with: dart create project_1_dart

---
+++ project-1-dart/bin/project_1_dart.dart
@@ -0,0 +1,6 @@
+import 'package:project_1_dart/project_1_dart.dart' as project_1_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_1_dart.calculate()}!');
+  print(arguments);
+}
